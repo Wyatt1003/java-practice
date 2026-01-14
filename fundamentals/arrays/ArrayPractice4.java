cd433a38537a5b29f84b9e05854e67d25f44857e
Tariq, the Loneliest Poet <165170205+Wyatt1003@users.noreply.github.com>
2026-01-14 16:37:24 +0000

    ArrayPractice4#1

---
+++ fundamentals/arrays/ArrayPractice4.java
@@ -0,0 +1,22 @@
+public class ArrayPractice4 {
+    public static void main(String[] args) {
+        //1 of 8
+        double[] winners = new double[8];
+
+        //2 of 8
+        int[] first_array = {1,3,5,7,11,13,17,19,23,29};
+
+        //3 of 8
+        int[] num = new int[20];
+        for (int i=0; i < 20; i++){
+            num[i] = i*2;
+        }
+
+        //4 of 8
+        int[][] maze = new int[30][40];
+
+        //5 of 8
+        int[][] nums = new int[16][32];
+        for (i=0;i < 16;)
+    }
+}
